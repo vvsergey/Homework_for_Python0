26a56cbeab8fa2159d5c1027eaf8a6c1b476a7f2
VSergeev <vvsergeev87@gmail.com>
2023-05-07 16:08:55 +0300

    lost time

---
+++ lost_time.py
@@ -0,0 +1,10 @@
+def find_time(hour:int, minutes:int) -> bool:
+    if hour <=24 and minutes < 60:
+        return True
+    return False
+
+if __name__ == '__main__':
+    hour = int(input())
+    minutes = int (input())
+    print(find_time(hour, minutes))
+    
